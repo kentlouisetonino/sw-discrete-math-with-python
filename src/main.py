84c6eb4b2b83b2c7fdec2a21545e65ce0a98709e
Kent Tonino <tonino.kentlouise@gmail.com>
2025-04-02 01:02:47 +0800

    Example of union.

--- src/main.py
+++ src/main.py
@@ -1,1 +1,4 @@
-print("hello");
+from module_01_introduction_to_discrete_math import code_set_theory
+
+# Module 01: Introduction to Discrete Mathematics.
+code_set_theory.union()
---
+++ src/module_01_introduction_to_discrete_math/code_set_theory.py
@@ -0,0 +1,5 @@
+def union():
+    set_car_a = { 'honda', 'toyota', 'mazda'}
+    set_car_b = { 'mazda', 'lamborghini', 'ferrari' }
+    set_car_union = set_car_a | set_car_b
+    print(set_car_union)
